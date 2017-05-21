2c8639e7d1f38afa5e57e97a8d2f9bfec8dd0e99
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-05-21 08:58:13 +0530

    Vijay - 2017/05/21 - java collections examples

--- collections-generics/src/org/pjay/collection/hashcode/app2/TestHashMap.java
+++ collections-generics/src/org/pjay/collection/hashcode/app2/TestHashMap.java
@@ -9,6 +9,15 @@ import java.util.Map;
 /**
  * @author vijayk
  *
+ * IMP:
+ * All objects being put to HashMap are stored in instance variable table as an array, with hash code representing the index, where to place the object in array.
+ * Initial capacity of the HashMap start with 16.
+ * When collisions occur with objects having same hashcode, then HashMap maintains those objects as single linked list.
+ * Where the first object put in an index is added as head of the linked list, when another object put in the same index it is added as next object. Similarly remaining.
+ * For more details verify the below link for getting a better understanding of this example in the form of diagram.
+ * For this reason HashMap doesn't provide objects in insertion order and gives data based on index and collision objects in same index 
+ * 
+ * https://github.com/konduruvijaykumar/java-examples/blob/master/collections-generics/org.pjay.collection.hashcode.app2.HashMap.pdf
  */
 public class TestHashMap {
 
