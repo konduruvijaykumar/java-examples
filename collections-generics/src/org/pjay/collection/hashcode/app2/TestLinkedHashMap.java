ec68b6b50a0f2c19b6523219ede839faee482537
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-05-22 13:08:31 +0530

    Vijay - 2017/05/22 - java collections examples

--- collections-generics/src/org/pjay/collection/hashcode/app2/TestLinkedHashMap.java
+++ collections-generics/src/org/pjay/collection/hashcode/app2/TestLinkedHashMap.java
@@ -9,6 +9,18 @@ import java.util.Map;
 /**
  * @author vijayk
  *
+ * IMP:
+ * All objects being put to LinkedHashMap are stored in instance variable table as an array, with hash code representing the index, where to place the object in array.
+ * Initial capacity of the LinkedHashMap start with 16 and maintains extra variables like "head, tail". The head (eldest) and tail (youngest).
+ * Entry of LinkedHashMap extends from HashMap.Node and adds extra fields "before, after". Which are used for maintaining the doubly-linked list relation.
+ * When collisions occur with objects having same hashcode.
+ * Then LinkedHashMap maintains those objects as doubly-linked list by referring it to after from previously added object and before of the object refers to previously added object.
+ * Where the first object put in an index is added as head of the LinkedHashMap.
+ * For more details verify the below link for getting a better understanding of this example in the form of diagram.
+ * For this reason LinkedHashMap provides objects in insertion order and gives data based on the order inserted.
+ * 
+ * https://github.com/konduruvijaykumar/java-examples/blob/master/collections-generics/org.pjay.collection.hashcode.app2.LinkedHashMap.pdf
+ * http://www.javarticles.com/2012/06/linkedhashmap.html
  */
 public class TestLinkedHashMap {
 	
